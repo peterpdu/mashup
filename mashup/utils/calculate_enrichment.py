26fd85a3be8ce73dd58a43086dcb6622304e0f7c
Peter Du <pdu@wmd43-20d.broadinstitute.org>
2020-07-08 17:16:11 -0400

    enrichment caller script

---
+++ mashup/utils/calculate_enrichment.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3.6
+
+import os, argparse
+
+import numpy as np
+import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from ..eval.enrichment import hart_enrichment, pan_enrichment, wainberg_enrichment
+
+mpl.rc('pdf', fonttype=42)
+mpl.rc('font', family='sans-serif')
+mpl.rc('font', serif='Helvetica')
+mpl.rcParams['pdf.use14corefonts'] = True
+mpl.rcParams['axes.unicode_minus'] = False
+
+
+def getParser():
+    parser = argparse.ArgumentParser()
+    req = parser.add_argument_group('required')
+    opt = parser.add_argument_group('optional')
+    req.add_argument('--inputs',
+                     help='square weighted adjacency npy(s)',
+                     type=str,
+                     required=True,
+                     nargs='+')
+    req.add_argument('--genes',
+                     help='gene list for npy',
+                     type=str,
+                     required=True)
+    req.add_argument('--database',
+                     help='edge list or gene x class annotation database',
+                     type=str,
+                     required=True)
+
+    opt.add_argument('--prefix',
+                     help='output prefix file label',
+                     type=str,
+                     default=None)
+    opt.add_argument('--method',
+                     help='enrichment method; default=pan2018',
+                     choices=('hart2017', 'pan2018', 'wainberg'),
+                     default='pan2018')
+    return parser
+
+
+if __name__ == '__main__':
+    args = getParser().parse_args()
+    infiles = args.inputs
+    outdir = os.path.dirname(infiles[0])
+    method = args.method
+
+    # load genes
+    genes = pd.read_table(args.genes, header=None)[0].tolist()
+
+    # load annotations
+    with open(args.database, 'r') as handle:
+        line = handle.readline()
+        s = len(line.split('\t'))
+        handle.close()
+    if s == 3:
+        annot = pd.read_table(args.database)
+    elif s > 3:
+        annot = pd.read_table(args.database, index_col=0)
+    else:
+        raise ValueError('Incorrect database format detected')
+
+    print('Calculating enrichments')
+    results = []
+    for f in infiles:
+        print(f)
+        df = np.load(f)
+        df = pd.DataFrame(df, index=genes, columns=genes)
+
+        results = []
+        if method == 'hart2017':
+            enrichment = hart_enrichment(df, annot)
+        elif method == 'pan2018':
+            enrichment = pan_enrichment(df, annot)
+        else:   # wainberg
+            enrichment = wainberg_enrichment(df, annot)
+        results.append(enrichment)
+
+    # save results
+    names = [os.path.basename(os.path.splitext(_)[0]) for _ in args.infiles]
+    results = pd.DataFrame(results, index=names)
+    outpath = os.path.join(outdir, 'enrichments.txt')
+    print('Writing results to {}'.format(outpath))
+    results.to_csv(outpath, sep='\t')
+
+    # save figure
+    for i in results.index:
+        v = results.loc[i].values
+        plt.plot(range(len(v)), v, label=i)
+    plt.legend()
+    plt.ylabel('{} enrichment'.format(method))
+    plt.title(os.path.basename(os.path.splitext(args.database)[0]))
+    
+    outpath = os.path.join(outdir, 'enrichments.pdf')
+    print('Saving figure to {}'.format(outpath))
+    plt.savefig(outpath)
