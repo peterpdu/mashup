753bbcd7e49562c7de094e4f25ae467fd8fa7bba
Peter Du <pdu@wmd43-20d.broadinstitute.org>
2020-07-12 00:51:25 -0400

    different similarity metrics added

--- scripts/vec_similarity.py
+++ scripts/vec_similarity.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3.6
 
 import sys, os
-
 import numpy as np
-import pandas as pd
-
-
-def correlate(a, method='pearson'):
-    pairwise = a.corr(method=method)
-    return pairwise
+from scipy.spatial import distance
 
 
 if __name__ == '__main__':
+    # metric = 'cosine'
+    # metric = 'correlation'
+    # metric = 'dotprod'
+    # metric = 'euclidean'
+    metric = sys.argv[2]
+    assert metric in ['cosine', 'correlation', 'dotprod', 'euclidean']
+
     f = sys.argv[1]
-    outpath = os.path.splitext(f)[0] + '_corr.npy'
+    outpath = os.path.splitext(f)[0] + '_{metric}_corr.npy'
+
+    # assumes data is features x genes
     a = np.load(f)
-    df = pd.DataFrame(a)
-    p = correlate(df)
-    np.fill_diagonal(p.values, 0)
-    np.save(outpath, p.values)
+    if metric == 'dotprod':
+        p = distance.squareform(distance.pdist(a.T, metric='cosine'))
+        p = p * np.dot(a.T, a)
+    else:
+        p = distance.squareform(distance.pdist(a.T, metric=metric))
+    np.fill_diagonal(p, 0)
+    np.save(outpath, p)
