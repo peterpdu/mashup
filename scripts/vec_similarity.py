aa2b0dca6f586df84d0c2509dca5292da02cdf5e
Peter Du <pdu@wmd43-20d.broadinstitute.org>
2020-07-16 20:35:21 -0400

    fix vec_similiarity to have higher scores mean better interaction

--- scripts/vec_similarity.py
+++ scripts/vec_similarity.py
@@ -15,17 +15,22 @@ if __name__ == '__main__':
     assert metric in ['cosine', 'correlation', 'dotprod', 'euclidean']
 
     f = sys.argv[1]
-    outpath = os.path.splitext(f)[0] + f'_{metric}_corr.npy'
+    outpath = os.path.splitext(f)[0] + f'_{metric}.npy'
 
     # assumes data is features x genes
     if f.endswith('.npy'):
         a = np.load(f)
     else:
         a = pd.read_table(f, index_col=0).values
-    if metric == 'dotprod':
+
+    if metric == 'correlation':
+        p = np.corrcoef(a.T)
+    elif metric == 'dotprod':
         p = distance.squareform(distance.pdist(a.T, metric='cosine'))
         p = p * np.dot(a.T, a)
+        p = np.log10(1/p + 1)
     else:
         p = distance.squareform(distance.pdist(a.T, metric=metric))
+        p = np.log10(1/p + 1)
     np.fill_diagonal(p, 0)
     np.save(outpath, p)
