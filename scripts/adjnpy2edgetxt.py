569175d10c668cbe710fdb25dd7131327df980e9
Peter Du <pdu@wmd43-20d.broadinstitute.org>
2020-07-16 22:05:56 -0400

    adj to edge script import fix

--- scripts/adjnpy2edgetxt.py
+++ scripts/adjnpy2edgetxt.py
@@ -4,7 +4,14 @@ import sys, os
 import numpy as np
 import pandas as pd
 
-from mashup.utils.matrix_convert import adj_to_edge
+
+def adj_to_edge(a, undirected=True):
+    ac = a.copy().astype(float)
+    if undirected:
+        ac.values[np.tril_indices(ac.shape[0], -1)] = np.NaN
+    e = ac.stack().reset_index()
+    e.columns = ['source', 'target', 'weight']
+    return e
 
 
 # adjnpy2edgetxt.npy input.npy genes.txt
@@ -15,5 +22,5 @@ if __name__ == '__main__':
 
     e = adj_to_edge(a)
 
-    outpath = os.path.splitext(sys.argv[1] + '_edge.txt')
+    outpath = os.path.splitext(sys.argv[1])[0] + '_edge.txt'
     e.to_csv(outpath, sep='\t', header=None, index=None)
